cbebbeebdbfa2b21c3d59ce321597ff0869c8107
Pablo Zuta <pablo.zuta.h@gmail.com>
2023-02-03 16:55:00 -0400

    se agregan posts epoch , time, number parsing, random numbers, time formating/parsing

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,7 +28,7 @@ export default function Home({ posts }: { posts: PostMeta[] }) {
 
 export async function getStaticProps() {
   const posts = getAllPosts()
-    .slice(0, 50)
+    .slice(0, 70)
     .map((post) => post.meta);
 
   return { props: { posts } };
